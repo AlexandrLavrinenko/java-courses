ba7e3e4c707cdd248ea61606fc52c8685df33d24
caballeroGISH <alexandr.lavrinenko@gmail.com>
2017-09-28 22:56:42 +0300

    added javaDoc
    del class-file

--- chapter_001/src/ru/job4j/Calculate.java
+++ chapter_001/src/ru/job4j/Calculate.java
@@ -1,9 +1,15 @@
 package ru.job4j;
 
+    /**
+     * Class Calculate. Тестовое задание. Вывод на консоль фразы "Hello, world"
+	 * @autor alavrinenko
+	 * @return String value
+     */
 public class Calculate {
     /**
-     * Тестовое задание.
-	 * Вывод на консоль фразы "Hello, world"
+     * Главный метод для вывода текстовой строки в консоль
+	 * @param строка для вывода в консоль (str)
+	 * @return String value
      */
     public static void main(String[] arg) {
 		String str = "Hello, world";
---
+++ chapter_001/src/ru/job4j/package-info.java
@@ -0,0 +1,10 @@
+/**
+* Package for calculate task.
+*
+* @author alavrinenko (mailto:alexandr.lavrinenko@gmail.com)
+* @version $Id$
+* @since 0.1
+*/
+
+
+package ru.job4j;
