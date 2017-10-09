0ae5a83966fd96397f9d8fc2d7eeda023bd040a7
caballeroGISH <alexandr.lavrinenko@gmail.com>
2017-10-09 22:56:08 +0300

    add new Calculator with tests

---
+++ chapter_001/src/main/ru/job4j/calculator/Calculator.java
@@ -0,0 +1,64 @@
+package ru.job4j.calculator;
+
+/**
+ * Class Calculate.
+ * Тестовое задание - выполнение арифметических действий над двумя значениями.
+ *
+ * @autor alavrinenko.
+ * @return double result.
+ */
+public class Calculator {
+    /**
+     * Результат вычисления
+     */
+    private double result;
+
+    /**
+     * Method add.
+     * Суммируем два значения.
+     * @param first .
+     * @param second .
+     */
+    public void add(double first, double second) {
+        this.result = first + second;
+    }
+
+    /**
+     * Method subtract.
+     * Вычитаем из первого второе значение.
+     * @param first .
+     * @param second .
+     */
+    public void subtract(double first, double second) {
+        this.result = first - second;
+    }
+
+    /**
+     * Method div.
+     * Делим первое значение на второе.
+     * @param first .
+     * @param second .
+     */
+    public void div(double first, double second) {
+        this.result = first / second;
+    }
+
+    /**
+     * Method multip.
+     * Умножаем первое значение на второе.
+     * @param first .
+     * @param second .
+     */
+    public void multip(double first, double second) {
+        this.result = first * second;
+    }
+
+    /**
+     * Method getResult.
+     * Получаем результат математических действий
+     * @return double result.
+     */
+    public double getResult() {
+        return this.result;
+    }
+}
