52435cbc938ac99e52a7edbe4094b276930784cd
caballeroGISH <alexandr.lavrinenko@gmail.com>
2017-10-14 00:56:28 +0300

    add task #192 - sum of even numbers in the range

---
+++ chapter_001/src/main/ru/job4j/loop/Counter.java
@@ -0,0 +1,27 @@
+package ru.job4j.loop;
+
+/**
+ * Class Counter.
+ * Тестовое задание - подсчет суммы чётных чисел в диапазоне.
+ *
+ * @autor alavrinenko.
+ * @return double result.
+ */
+public class Counter {
+    /**
+     * Method max.
+     * Выполняем подсчет суммы чётных чисел в диапазоне.
+     * @param start - начальная позиция.
+     * @param finish - конечная позиция.
+     * @return int result.
+     */
+    public int add(int start, int finish) {
+        int sum = 0;
+        for (int i = start; i <= finish; i++) {
+            if (i % 2 == 0) {
+                sum = sum + i;
+            }
+        }
+        return sum;
+    }
+}
---
+++ chapter_001/src/main/ru/job4j/loop/package-info.java
@@ -0,0 +1,10 @@
+/**
+* Package for loop task.
+*
+* @author alavrinenko (mailto:alexandr.lavrinenko@gmail.com).
+* @version $Id$.
+* @since 0.1.
+*/
+
+
+package ru.job4j.loop;
